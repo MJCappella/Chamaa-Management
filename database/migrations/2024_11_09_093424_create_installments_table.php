4d4d8d69aec0f78f7846ca6db857719370069f6e
KipCollo <kipkosgei.tech@gmail.com>
2024-11-09 12:36:49 +0300

    add Loan and Installment models

--- database/migrations/2024_11_09_093424_create_installments_table.php
+++ database/migrations/2024_11_09_093424_create_installments_table.php
@@ -11,15 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('installments', function (Blueprint $table) {
             $table->id();
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('phoneNumber')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('Amount');
+            $table->foreignId('loan_id');
             $table->timestamps();
         });
     }
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('installments');
     }
 };
